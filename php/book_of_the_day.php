c2bcdb95fccd537ec6afdda0cb85e554d9c951cf
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-08-16 11:39:14 +0100

    Update book_of_the_day.php

--- php/book_of_the_day.php
+++ php/book_of_the_day.php
@@ -1,5 +1,31 @@
 <?php
 namespace SIM\LIBRARY;
-use SIM;
+use SIM;    
 
-$selectedBook = 
+function filterWhereContentNotEmpty( $where = '' ) {
+        $where .= " AND post_content IS NOT NULL AND post_content != ''";
+        return $where;
+    }
+
+add_filter( 'posts_where', __NAMESPACE__.'\filterWhereContentNotEmpty' );
+
+$selectedBook = get_posts(
+            array(
+                'post_type'              => 'book',
+                'numberposts' => 1,
+    'orderby'     => 'rand',
+    'post_status' => 'publish',
+    'meta_query'     => array(
+        array(
+            'key'     => 'picture',     // Replace with your custom field key
+            'compare' => 'EXISTS',                 
+        ),
+        array(
+            'key'     => 'url',     // Replace with your custom field key
+            'compare' => 'EXISTS',                 
+        ),
+    ),
+            )
+        );
+
+remove_filter( 'posts_where', __NAMESPACE__.'\filterWhereContentNotEmpty' );
