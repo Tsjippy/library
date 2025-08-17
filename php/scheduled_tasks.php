b45f4dac1b37ea6b1823fbb22dda5170c3e45a9d
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-08-17 06:37:36 +0100

    Update scheduled_tasks.php

--- php/scheduled_tasks.php
+++ php/scheduled_tasks.php
@@ -1,3 +1,19 @@
+<?php
+namespace SIM\LIBRARY;
+use SIM;
 
+add_action('init', __NAMESPACE__.'\initTasks');
+function initTasks(){
+	//add action for use in scheduled task
+	add_action( 'send_book_of_the_day', __NAMESPACE__.'\bookOfTheDay' );
+}
+
+function scheduleTasks(){
+    
+    $freq   = SIM\getModuleOption(MODULE_SLUG, 'book_of_the_day_freq');
+    if($freq){
+        SIM\scheduleTask('send_book_of_the_day', $freq);
+    }
+}
 
 $book = bookOfTheDay();
