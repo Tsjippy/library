96361e696cd7f10ada5b5842f6738e7946c5371a
Tsjippy <ewaldha@hotmail.com>
2026-06-01 13:22:08 +0200

    fixed: added domain to __ function

--- templates/archive-books.php
+++ templates/archive-books.php
@@ -99,8 +99,8 @@ function displayBookArchive(){
 				'format' 	=> '/page/%#%',
 				'current' 	=> $currentPage,
 				'total' 	=> $totalPages,
-				'prev_text' => __('« prev'),
-				'next_text' => __('next »'),
+				'prev_text' => __('« prev', 'tsjippy'),
+				'next_text' => __('next »', 'tsjippy'),
 			));
 		}
 	}else{
