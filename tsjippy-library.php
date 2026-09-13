2a65c04660cc4b3e17917373db2e0c6b34a56abe
Tsjippy <ewaldha@hotmail.com>
2026-09-13 06:42:38 +0200

    skip:

--- tsjippy-library.php
+++ tsjippy-library.php
@@ -10,9 +10,9 @@ namespace TSJIPPY\LIBRARY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    7.1
  * Requires PHP:         8.3
- * Plugin URI:            https://github.com/Tsjippy/library
+ * Plugin URI:           https://github.com/Tsjippy/library
  * Tested:                7.1
- * TextDomain:            tsjippy
+ * TextDomain:           tsjippy
  * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
