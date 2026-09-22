8b45ea3dc94e6663eaa838fd74d3a54610db6156
Tsjippy <ewaldha@hotmail.com>
2026-09-22 08:37:35 +0200

    changed: js refactor

--- php/enqueue.php
+++ php/enqueue.php
@@ -18,7 +18,15 @@ add_action('admin_enqueue_scripts', __NAMESPACE__ . '\loadAssets');
 function loadAssets()
 {
     wp_register_style('tsjippy_library_style', TSJIPPY\pathToUrl(PLUGINPATH . 'css/library.min.css'), array(), PLUGINVERSION);
-    wp_register_script_module('@tsjippy/library_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/library' . TSJIPPY\JSEXTENSION), ['@tsjippy/formsubmit_script'], PLUGINVERSION);
+
+    $deps   = SCRIPT_DEBUG ? [  
+        '@tsjippy/form_submit_functions', 
+        "@tsjippy/form_exports", 
+        "@tsjippy/show_loader", 
+        "@tsjippy/display_message"
+    ] :
+    [];
+    wp_register_script_module('@tsjippy/library_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/library' . TSJIPPY\JSEXTENSION), $deps, PLUGINVERSION);
 
     add_filter( 'script_module_data_@tsjippy/library_script', function($data){
         $data['ajaxUrl'] = admin_url( 'admin-ajax.php' );
